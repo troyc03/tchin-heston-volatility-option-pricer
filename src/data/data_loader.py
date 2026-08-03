6c23b96c60f62bf0554036fc47567fd2c907d52f
Troy Chin <tchin@csu.fullerton.edu>
2026-08-03 15:00:30 -0700

    Updated EDA notebook

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -3,7 +3,7 @@ import yfinance as yf
 import pandas as pd
 
 ticker = yf.Ticker("MSFT")
-df = ticker.history(start='2020-01-01')
+df = ticker.history(period='max')
 df.to_csv('msft_data.csv')
 
 print('Dataframe conversion complete!')
