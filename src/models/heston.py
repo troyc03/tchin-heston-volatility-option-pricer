59eb60fc03d4609a8baff081138defda36cb665a
Troy Chin <tchin@csu.fullerton.edu>
2026-08-03 00:19:17 -0700

    Removed unnecessary files

--- src/models/heston.py
+++ src/models/heston.py
@@ -1,4 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
 
 class HestonModel:
+    def __init__(kappa, theta, sigma, rho, r, q, K, T):
+        pass
+
+    def gbm_simulator():
+        pass
+
+class HestonPDE:
+    def pde_solver():
+        pass
+
+def main():
+    print(f'European Call Option Price: {...}')
+    print(f'European Put Option Price: {...}')
+    pass
+
+if __name__ == '__main__':
+    main()
