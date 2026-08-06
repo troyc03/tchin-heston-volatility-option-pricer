853e96c63b3c39bf4e976d5e66c053b635a56ab9
Troy Chin <tchin@csu.fullerton.edu>
2026-08-06 01:41:37 -0700

    Preparing Neural SDE script

---
+++ src/models/heston_sde.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+from scipy.integrate import quad
+import torch.nn as nn
+import torch 
+
+class NeuralSDE(nn.Module):
+    def __init__(self):
+        super().__init__()
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
